4474059a8d080de64702da56cd5e049e939dbef9
VeXuZ <vexuzdorotan@gmail.com>
2020-07-28 23:57:02 +0800

    Routes

--- mern_project/frontend/src/user/components/UsersList.js
+++ mern_project/frontend/src/user/components/UsersList.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import UserItem from "./UserItem";
-import Card from "../../shared/components/UIElements/Card";
-import "./UsersList.css";
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
 
-const UsersList = (props) => {
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
         <Card>
-          <h2>No users found</h2>
+          <h2>No users found.</h2>
         </Card>
       </div>
     );
   }
 
   return (
-    <ul>
-      {props.items.map((user) => (
+    <ul className="users-list">
+      {props.items.map(user => (
         <UserItem
           key={user.id}
           id={user.id}
--- mern_project/frontend/src/user/pages/Users.js
+++ mern_project/frontend/src/user/pages/Users.js
@@ -1,20 +1,16 @@
-import React from "react";
-import UsersList from "../components/UsersList";
+import React from 'react';
+
+import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
     {
-      id: "u1",
-      name: "name1",
-      image: `https://picsum.photos/seed/u1/200/300`,
-      places: 3,
-    },
-    {
-      id: "u2",
-      name: "name2",
-      image: "https://picsum.photos/seed/u2/200/300",
-      places: 5,
-    },
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
   ];
 
   return <UsersList items={USERS} />;
