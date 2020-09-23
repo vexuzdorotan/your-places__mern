12d2772cbd1d95eec7316531eb18450901a09307
VeXuZ <vexuzdorotan@gmail.com>
2020-09-23 17:37:56 +0800

    Deploy with Heroku, Firebase

---
+++ mern_project/backend/public/precache-manifest.035218c5055ad191e96a11d02c4ee5de.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef474e9c037acf786cc2173028d9e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc16afb30e2a9d33d4c6",
+    "url": "/static/css/0.352f0a6b.chunk.css"
+  },
+  {
+    "revision": "f3fc8e175b793049c2a8",
+    "url": "/static/css/4.8383b8e6.chunk.css"
+  },
+  {
+    "revision": "73a6b37b70e45e68090a",
+    "url": "/static/css/5.3f0ef8ee.chunk.css"
+  },
+  {
+    "revision": "be067817c940c27b9562",
+    "url": "/static/css/6.25608a53.chunk.css"
+  },
+  {
+    "revision": "b60fb06db42cda5ebd54",
+    "url": "/static/css/7.6cafb8e4.chunk.css"
+  },
+  {
+    "revision": "276cd67cfe21f4ad1c0f",
+    "url": "/static/css/8.d55f2f7f.chunk.css"
+  },
+  {
+    "revision": "ec5f4613d68bf959789d",
+    "url": "/static/css/main.2a2b113a.chunk.css"
+  },
+  {
+    "revision": "dc16afb30e2a9d33d4c6",
+    "url": "/static/js/0.3a97b22a.chunk.js"
+  },
+  {
+    "revision": "fc2098d8696f6dc6d8ba",
+    "url": "/static/js/3.9eb3540a.chunk.js"
+  },
+  {
+    "revision": "f3fc8e175b793049c2a8",
+    "url": "/static/js/4.cbb7b602.chunk.js"
+  },
+  {
+    "revision": "73a6b37b70e45e68090a",
+    "url": "/static/js/5.9e4c5fdc.chunk.js"
+  },
+  {
+    "revision": "be067817c940c27b9562",
+    "url": "/static/js/6.42a92b02.chunk.js"
+  },
+  {
+    "revision": "b60fb06db42cda5ebd54",
+    "url": "/static/js/7.8e4dd5b6.chunk.js"
+  },
+  {
+    "revision": "276cd67cfe21f4ad1c0f",
+    "url": "/static/js/8.efd80082.chunk.js"
+  },
+  {
+    "revision": "ec5f4613d68bf959789d",
+    "url": "/static/js/main.47d03b0e.chunk.js"
+  },
+  {
+    "revision": "aaf8a638e9d160e10012",
+    "url": "/static/js/runtime-main.760e7fb2.js"
+  }
+]);
